6b2e7cd067e06ed34f432341af3e6e0bdad8c8de
Xioth <kxioth@hotmail.com>
2024-04-20 17:43:39 -0400

    import bootstrap in app.

--- Web/cyril-buee/src/App.js
+++ Web/cyril-buee/src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table className="table table-dark table-sm table-hover">
+          <thead>
+            <tr>
+              <th>Test</th>
+              <th>Test</th>
+              <th>Test</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
         <a
           className="App-link"
           href="https://reactjs.org"
