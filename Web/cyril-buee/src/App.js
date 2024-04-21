46ee5b90c3e61712a6c635fc5518615b90e99e36
Xioth <kxioth@hotmail.com>
2024-04-21 08:41:09 -0400

    Change Bootstrap for Tailwind.

--- Web/cyril-buee/src/App.js
+++ Web/cyril-buee/src/App.js
@@ -1,28 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <table className="table table-dark table-sm table-hover">
-          <thead>
-            <tr>
-              <th>Test</th>
-              <th>Test</th>
-              <th>Test</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
+        <h1 className='text-3xl font-bold underline'>
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
